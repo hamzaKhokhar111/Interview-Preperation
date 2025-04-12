ddc8e192de13a4896dde87b0fbf0503efee27957
unknown <hamzaashrafkh111@gmail.com>
2025-04-12 14:41:31 +0500

    100 objects of  javascript

---
+++ Events/Event.js
@@ -0,0 +1,28 @@
+// JavaScript Events Kya Hote Hain?
+// Event ka matlab hota hai user ya browser ka action — jaise:
+
+// Button click
+// Mouse move
+// Key press
+// Page load
+// Form submit
+// Resize, scroll, etc.
+
+// JavaScript in events ko detect kar ke react kar sakta hai using Event Listeners.
+
+// Using HTML DOM Property
+    // const btn = document.getElementById("myBtn");
+    // btn.onclick = function () {
+    //   alert("Button Clicked");
+    // };
+    
+    
+    // Using addEventListener (Preferred Way ✅)
+//     const btn = document.getElementById("myBtn");
+// btn.addEventListener("click", function () {
+//   alert("Button Clicked!");
+// });
+
+
+
+
